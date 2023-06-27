b5a52efb87080ebb315463e30e49937ea3ad7914
Dewa Bisma <idewagedebisma@gmail.com>
2023-06-27 19:15:29 +0700

    setup tabview, routes

---
+++ SwiftAnimation/Views/C4TabView.swift
@@ -0,0 +1,64 @@
+//
+//  C4TabView.swift
+//  SwiftAnimation
+//
+//  Created by Bisma Mahendra I Dewa Gede on 27/06/23.
+//
+
+import SwiftUI
+
+enum NavigationTabs: String {
+    case playground = "Playground"
+    case useCase = "UseCase"
+}
+
+struct C4TabView: View {
+    @Binding var selectedTab: NavigationTabs
+    
+    @ObservedObject var useCaseNavigation: UseCaseNavigationManager
+    
+    var body: some View {
+        TabView(selection: $selectedTab) {
+            AnimationPlaygroundScreen()
+            .tabItem {
+                Label {
+                    Text("Playground")
+                } icon: {
+                    Image(systemName: "rectangle.3.group.fill")
+                }
+            }
+            .tag(NavigationTabs.playground)
+            
+            NavigationStack(path: $useCaseNavigation.paths) {
+                ChartAnimateView()
+                    .navigationDestination(for: UseCaseRoute.self) { route in
+                        switch(route) {
+                        case .chartOne:
+                            ChartCustomAnimateView()
+                        case .chartTwo:
+                            ChartCustomAnimateView()
+                        }
+                    }
+            }
+            .tabItem {
+                Label {
+                    Text("Use Case")
+                } icon: {
+                    Image(systemName: "text.book.closed.fill")
+                }
+            }
+            .tag(NavigationTabs.useCase)
+            .environmentObject(useCaseNavigation)
+        }
+        .preferredColorScheme(.light)
+        .gesture(TapGesture(count: 2).onEnded {
+            switch selectedTab {
+            case .playground:
+                break
+            case .useCase:
+                useCaseNavigation.reset()
+                break
+            }
+        })
+    }
+}
